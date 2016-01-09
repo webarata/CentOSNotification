77b5a3525ba6fafef55c3997c3e35da635282fc4
webarata <arata.shinichi@gmail.com>
2016-01-09 16:40:10 +0900

    content scriptの追加

---
+++ contentScript.js
@@ -0,0 +1,7 @@
+var html = document.getElementsByTagName('body')[0].innerHTML;
+
+html = html.replace(/Moderate/g, '<span style="color: black;">Moderate</span>');
+html = html.replace(/Important/g, '<span style="color: red;">Important</span>');
+html = html.replace(/Critical/g, '<span style="color: red; background-color: yellow;">Critical</span>');
+
+document.getElementsByTagName('body')[0].innerHTML = html;
