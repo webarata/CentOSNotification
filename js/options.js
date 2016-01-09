1b4dc6a6695c2efc78ef7a80afb230f6e05f57d7
webarata3 <webarata3@gmail.com>
2016-01-10 08:05:58 +0900

    ハイライトの表示設定の追加

--- js/options.js
+++ js/options.js
@@ -8,6 +8,17 @@ notification[1].addEventListener('change', function() {
   localStorage['notification'] = 'false';
 });
 
+var highlight = document.getElementsByName('highlight');
+
+highlight[0].addEventListener('change', function() {
+  localStorage['highlight'] = true;
+});
+
+highlight[1].addEventListener('change', function() {
+  localStorage['highlight'] = false;
+});
+
 document.body.onload = function() {
   localStorage['notification'] === 'true' ? notification[0].checked = true : notification[1].checked = true;
+  localStorage['highlight'] === 'true' ? highlight[0].checked = true : highlight[1].checked = true;
 };
