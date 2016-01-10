d50cf74bc327cd40693cfb1c16262966219f07ce
webarata <arata.shinichi@gmail.com>
2016-01-11 07:52:08 +0900

    contentScript.jsを即時関数に変更

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -13,14 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-chrome.runtime.sendMessage({method: 'getHighlight'}, function(response) {
-  if (response.highlight != null && response.highlight === 'true') {
-    var html = document.getElementsByTagName('body')[0].innerHTML;
+var CentOSNotif = CentOSNotif || {};
 
-    html = html.replace(/Moderate/g, '<span style="color: black;">Moderate</span>');
-    html = html.replace(/Important/g, '<span style="color: red;">Important</span>');
-    html = html.replace(/Critical/g, '<span style="color: red; background-color: yellow;">Critical</span>');
+(function() {
+  chrome.runtime.sendMessage({method: 'getHighlight'}, function(response) {
+    if (response.highlight != null && response.highlight === 'true') {
+      var html = document.getElementsByTagName('body')[0].innerHTML;
 
-    document.getElementsByTagName('body')[0].innerHTML = html;
-  }
-});
+      html = html.replace(/Moderate/g, '<span style="color: black;">Moderate</span>');
+      html = html.replace(/Important/g, '<span style="color: red;">Important</span>');
+      html = html.replace(/Critical/g, '<span style="color: red; background-color: yellow;">Critical</span>');
+
+      document.getElementsByTagName('body')[0].innerHTML = html;
+    }
+  });
+})();
