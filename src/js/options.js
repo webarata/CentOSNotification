419ef25f5853d3c2924f9d4321467b239a217ee2
webarata <arata.shinichi@gmail.com>
2016-01-11 07:24:57 +0900

    options.jsを即時関数に変更

--- src/js/options.js
+++ src/js/options.js
@@ -13,35 +13,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-var notification = document.getElementsByName('notification');
+var CentOSNotif = CentOSNotif || {};
 
-notification[0].addEventListener('change', function() {
-  localStorage['notification'] = 'true';
-});
+(function() {
+  var notification = document.getElementsByName('notification');
 
-notification[1].addEventListener('change', function() {
-  localStorage['notification'] = 'false';
-});
+  notification[0].addEventListener('change', function() {
+    localStorage['notification'] = 'true';
+  });
 
-var highlight = document.getElementsByName('highlight');
+  notification[1].addEventListener('change', function() {
+    localStorage['notification'] = 'false';
+  });
 
-highlight[0].addEventListener('change', function() {
-  localStorage['highlight'] = true;
-});
+  var highlight = document.getElementsByName('highlight');
 
-highlight[1].addEventListener('change', function() {
-  localstorage['highlight'] = false;
-});
+  highlight[0].addEventListener('change', function() {
+    localStorage['highlight'] = true;
+  });
 
-var intervalMinute = document.getElementById('intervalMinute');
+  highlight[1].addEventListener('change', function() {
+    localstorage['highlight'] = false;
+  });
 
-intervalMinute.addEventListener('input', function() {
-  this.value = this.value.replace(/[^0-9]/, '');
-  localStorage['intervalMinute'] = this.value === '' ? 60 : this.value;
-});
+  var intervalMinute = document.getElementById('intervalMinute');
 
-document.body.onload = function() {
-  localStorage['notification'] === 'true' ? notification[0].checked = true : notification[1].checked = true;
-  localStorage['highlight'] === 'true' ? highlight[0].checked = true : highlight[1].checked = true;
-  intervalMinute.value = localStorage['intervalMinute'] == null ? 60 : localStorage['intervalMinute'];
-};
+  intervalMinute.addEventListener('input', function() {
+    this.value = this.value.replace(/[^0-9]/, '');
+    localStorage['intervalMinute'] = this.value === '' ? 60 : this.value;
+  });
+
+  document.body.onload = function() {
+    localStorage['notification'] === 'true' ? notification[0].checked = true : notification[1].checked = true;
+    localStorage['highlight'] === 'true' ? highlight[0].checked = true : highlight[1].checked = true;
+    intervalMinute.value = localStorage['intervalMinute'] == null ? 60 : localStorage['intervalMinute'];
+  };
+})();
