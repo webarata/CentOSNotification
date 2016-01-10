cb42e6740954aa80bcac879af9d12fb49ba48a5a
webarata <arata.shinichi@gmail.com>
2016-01-11 06:37:44 +0900

    名前空間をCentOSNotifに変更

--- src/js/popup.js
+++ src/js/popup.js
@@ -18,7 +18,7 @@
 
   document.getElementById('newUrl').setAttribute('href', url);
 
-  document.getElementById('critical').innerText = CentOS.getCriticalCount();
-  document.getElementById('important').innerText = CentOS.getImportantCount();
-  document.getElementById('moderate').innerText = CentOS.getModerateCount();
+  document.getElementById('critical').innerText = CentOSNotif.getCriticalCount();
+  document.getElementById('important').innerText = CentOSNotif.getImportantCount();
+  document.getElementById('moderate').innerText = CentOSNotif.getModerateCount();
 })();
