8fe7d7082da520973428ffcde1a0f39553f525a3
webarata <arata.shinichi@gmail.com>
2016-01-11 13:30:36 +0900

    localStorageに設定画面の項目の追加

--- src/js/options.js
+++ src/js/options.js
@@ -21,33 +21,33 @@ var CentOSNotif = CentOSNotif || {};
   var notification = document.getElementsByName('notification');
 
   notification[0].addEventListener('change', function() {
-    localStorage['notification'] = 'true';
+    localStorage.notification = 'true';
   });
 
   notification[1].addEventListener('change', function() {
-    localStorage['notification'] = 'false';
+    localStorage.notification = 'false';
   });
 
   var highlight = document.getElementsByName('highlight');
 
   highlight[0].addEventListener('change', function() {
-    localStorage['highlight'] = true;
+    localStorage.highlight = 'true';
   });
 
   highlight[1].addEventListener('change', function() {
-    localStorage['highlight'] = false;
+    localStorage.highlight = 'false';
   });
 
   var intervalMinute = document.getElementById('intervalMinute');
 
   intervalMinute.addEventListener('input', function() {
     this.value = this.value.replace(/[^0-9]/, '');
-    localStorage['intervalMinute'] = this.value === '' ? 60 : this.value;
+    localStorage.intervalMinute = this.value === '' ? 60 : this.value;
   });
 
   document.body.onload = function() {
-    localStorage['notification'] === 'true' ? notification[0].checked = true : notification[1].checked = true;
-    localStorage['highlight'] === 'true' ? highlight[0].checked = true : highlight[1].checked = true;
-    intervalMinute.value = localStorage['intervalMinute'] == null ? 60 : localStorage['intervalMinute'];
+    localStorage.notification === 'true' ? notification[0].checked = true : notification[1].checked = true;
+    localStorage.highlight === 'true' ? highlight[0].checked = true : highlight[1].checked = true;
+    intervalMinute.value = localStorage.intervalMinute == null ? 60 : localStorage.intervalMinute;
   };
 })();
